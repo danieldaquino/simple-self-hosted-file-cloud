9c4648a5b0f17df86db562cd78cc5ea60a3d8138
Daniel D’Aquino <daniel@daquino.me>
2024-02-22 19:20:53 -0800

    Improve Tailscale login handling

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,22 @@ tailscaled &
 echo "Waiting for tailscaled to start"
 sleep 10
 echo "Logging in to tailscale"
-tailscale login --authkey=${TS_AUTHKEY}
-echo "Waiting for tailscale to log in"
-sleep 10
+# Check `tailscale status` to see if we're already logged in. If we are, don't log in again.
+# `tailscale status` returns status code 0 if logged in
+if tailscale status; then
+  echo "Already logged in to tailscale"
+else
+  echo "Not logged in to tailscale"
+  if [ -z "${TS_AUTHKEY}" ]; then
+    echo "No authkey provided"
+    tailscale login --qr
+  else
+    tailscale login --authkey=${TS_AUTHKEY}
+  fi
+  echo "Waiting for tailscale to log in"
+  sleep 10
+fi
+
 echo "Generating HTTPS certificates"
 tailscale cert --cert-file=/config/tailscale.crt --key-file=/config/tailscale.key ${TS_DOMAIN}
 echo "Done!"
